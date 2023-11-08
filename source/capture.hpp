040690074ebbd2d044a5f9cae186d94313dc5245
drew-k <drewkinney.03@gmail.com>
2023-11-07 23:21:17 -0500

    Get pointer to display struct

--- source/capture.hpp
+++ source/capture.hpp
@@ -1,8 +1,26 @@
 #ifndef CAPTURE_HPP
-#def CAPTURE_HPP
+#define CAPTURE_HPP
 
-#include <X11/xlib.h>
+#include <X11/Xlib.h>
+#include <iostream>
 
+class Capture
+{
 
+private:
+    Display* display;
+
+public:
+    Capture()
+    {
+        display = XOpenDisplay(NULL);
+        if (display == NULL)
+        {
+            std::cout << "Failed to get display" << std::endl;
+            exit(1);
+        }
+    }
+
+};
 
 #endif
