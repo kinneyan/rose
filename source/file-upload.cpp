bda407a03df6a6db98dbd30f87773c7428c0b9a0
kinneyan <drewkinney03@gmail.com>
2023-11-28 09:28:52 -0500

    Use callback write function to store response string from host

--- source/file-upload.cpp
+++ source/file-upload.cpp
@@ -20,8 +20,16 @@ Connection::~Connection()
     curl_easy_cleanup(curl);
 }
 
+size_t Connection::curlWriteFunction(char* ptr, size_t size, size_t nmemb, std::string* userData)
+{
+    size_t nlength = size * nmemb;
+    userData->append((char*) ptr, nlength);
+    return nlength;
+}
+
 int Connection::uploadFile(const char* fpath)
 {
+    std::string response;
     curl_easy_setopt(curl, CURLOPT_URL, hostURL.c_str());
 
     curl_mime* form = curl_mime_init(curl);
@@ -35,15 +43,26 @@ int Connection::uploadFile(const char* fpath)
     }
 
     field = curl_mime_addpart(form);
+    
     curl_mime_filedata(field, fpath);
     curl_mime_name(field, "file");
 
     curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
-    CURLcode response = curl_easy_perform(curl);
+
+    // don't return ok if curl failed
+    curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+
+    // use write callback function to response string instead of
+    // printing the output
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlWriteFunction);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    CURLcode responseCode = curl_easy_perform(curl);
 
     curl_mime_free(form);
 
-    std::cerr << curl_easy_strerror(response) << std::endl;
+    //std::cerr << curl_easy_strerror(response) << std::endl;
+    //std::cout << response << std::endl;
 
-    return (int) response;
+    return responseCode;
 }
--- source/file-upload.hpp
+++ source/file-upload.hpp
@@ -12,6 +12,8 @@ private:
     std::string hostURL;
     std::map<std::string, std::string> args;
 
+    static size_t curlWriteFunction(char* ptr, size_t size, size_t nmemb, std::string* userData);
+
 public:
     Connection(std::string host, std::map<std::string, std::string> args);
     ~Connection();
